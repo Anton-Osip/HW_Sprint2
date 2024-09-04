1eb2f3799fbf5e438def7dfec6afd1e4058d529d
Anton-Osip <osipchika@gmail.com>
2024-09-04 15:53:21 +0300

    Задание 8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,30 +7,19 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            if (action.payload === 'up') {
-                return [...state.sort((a: UserType, b: UserType) => {
-                    if (a.name > b.name) {
-                        return 1;
-                    }
-                    if (a.name < b.name) {
-                        return -1;
-                    }
-                    return 0;
-                })]
-            } else {
-                return [...state.sort((a: UserType, b: UserType) => {
-                    if (a.name > b.name) {
-                        return -1;
-                    }
-                    if (a.name < b.name) {
-                        return 1;
-                    }
-                    return 0;
-                })]
-            }
+            const newState = [...state].sort((a: UserType, b: UserType) => {
+                if (a.name > b.name) {
+                    return 1;
+                }
+                if (a.name < b.name) {
+                    return -1;
+                }
+                return 0;
+            })
+            return action.payload === 'up' ? newState : newState.reverse()
         }
         case 'check': {
-            return state.filter((a: UserType) => a.age > action.payload).sort((a, b) => b.age - a.age)
+            return state.filter((a: UserType) => a.age >= action.payload)
         }
         default:
             return state
