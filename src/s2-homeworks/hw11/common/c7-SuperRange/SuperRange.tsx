4ee114cab4413d076815ea1269ea98e48ed716a6
Anton-Osip <osipchika@gmail.com>
2024-09-23 16:33:27 +0300

    Задание 11

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -4,8 +4,10 @@ import {Slider, SliderProps} from '@mui/material'
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
+            sx = {{
+                width: '147px',
+                color: '#00CC22',
+
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
